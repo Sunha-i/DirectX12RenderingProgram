b2a9799d0686f6cf3542550133d9a8ba4d30e61f
Sunha <icecream20@khu.ac.kr>
2024-03-16 15:43:02 +0900

    Initialize window

--- Source/Library/Game.h
+++ Source/Library/Game.h
@@ -1,28 +1,13 @@
-﻿/*+===================================================================
-  File:      GAME.H
-
-  Summary:   Game header file that contains declarations of functinos
-             used for the lab samples of Game Graphics Programming
-             course.
-
-  Functions: PrintHi
-
-  © 2022 Kyung Hee University
-===================================================================+*/
-#pragma once
+﻿#pragma once
 
 #include "Common.h"
 
-namespace library
-{
-    /*--------------------------------------------------------------------
-      Forward declarations
-    --------------------------------------------------------------------*/
+LRESULT CALLBACK WindowProc
+(
+	_In_ HWND hWnd,
+	_In_ UINT uMsg,
+	_In_ WPARAM wParam,
+	_In_ LPARAM lParam
+);
 
-    /*F+F+++F+++F+++F+++F+++F+++F+++F+++F+++F+++F+++F+++F+++F+++F+++F+++F
-      Function: Render
-
-      Summary:  Render the frame
-    F---F---F---F---F---F---F---F---F---F---F---F---F---F---F---F---F-F*/
-    void PrintHi();
-}
+HRESULT InitWindow(_In_ HINSTANCE hInstance, _In_ INT nCmdShow);
